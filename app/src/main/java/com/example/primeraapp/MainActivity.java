5d752e8872cbd16975c8b02bb96cd6920eaf13d8
Kefren2019 <50784026+Kefren2019@users.noreply.github.com>
2019-05-24 21:29:37 -0500

    Se ingresa comentario

--- app/src/main/java/com/example/primeraapp/MainActivity.java
+++ app/src/main/java/com/example/primeraapp/MainActivity.java
@@ -14,7 +14,7 @@ import android.widget.TextView;
 import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
-    //comentario
+    //comentario rr
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
